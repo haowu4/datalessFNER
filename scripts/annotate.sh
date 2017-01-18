dc4c9c58bd87bc12f89ba05c175b5f22f1025503
Hao Wu <haowu@haowu.me>
2017-01-17 20:21:51 -0600

    annotate data

---
+++ scripts/annotate.sh
@@ -0,0 +1,3 @@
+mvn exec:java \
+	-Dexec.mainClass="edu.illinois.cs.cogcomp.finer.entry.AnnotationFiles" \
+	-Dexec.args="-input /home/haowu4/data/1blm/text/train/news.en-00001-of-00100 -output /tmp/dump_cache "
